1ea6bb39a8d4416fb0a8e876a14bd7be858c6f56
Albert Malina <albert.malina@gmail.com>
2018-02-13 18:07:11 +0100

    .bin: remove obsolete entries

--- .bin/launcher.sh
+++ .bin/launcher.sh
@@ -4,15 +4,6 @@
 TERM=i3-sensible-terminal
 
 case `basename $0` in
-	s_ekg)
-		$TERM -name EKG -title EKG -e bash -c "LANG=pl_PL luit ekg"
-		;;
-	s_mc)
-		$TERM -name MC -title MC -e bash -c "mc"
-		;;
-	s_mcroot)
-		gksudo -- $TERM -name MC -title MC -e bash -c "mc"
-		;;
-	*)
-		exit 1
+    *)
+        exit 1
 esac
