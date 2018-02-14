f10ac81733250bfbfab4d2fe00e457abac001a6d
Albert Malina <albert.malina@gmail.com>
2018-02-14 17:07:08 +0100

    aur.local/surf/config.h: download to /tmp instead of pwd

--- manual-steps/PRE-install_packages/aur.local/001-surf/config.h
+++ manual-steps/PRE-install_packages/aur.local/001-surf/config.h
@@ -78,7 +78,7 @@ static WebKitFindOptions findopts = WEBKIT_FIND_OPTIONS_CASE_INSENSITIVE |
 /* DOWNLOAD(URI, referer) */
 #define DOWNLOAD(u, r) { \
         .v = (const char *[]){ "i3-sensible-terminal", "-e", "/bin/sh", "-c",\
-             "curl -g -L -J -O -A \"$1\" -b \"$2\" -c \"$2\"" \
+             "cd /tmp; curl -g -L -J -O -A \"$1\" -b \"$2\" -c \"$2\"" \
              " -e \"$3\" \"$4\"; read", \
              "surf-download", useragent, cookiefile, r, u, NULL \
         } \
