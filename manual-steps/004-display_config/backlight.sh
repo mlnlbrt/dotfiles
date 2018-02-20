07496d01d4a7d406fb6b620b1b98e59c89a3faf1
Albert Malina <albert.malina@gmail.com>
2018-02-20 15:13:43 +0100

    backlight.sh: increase step

--- manual-steps/004-display_config/backlight.sh
+++ manual-steps/004-display_config/backlight.sh
@@ -6,13 +6,13 @@ max_bg=`cat $dev/max_brightness`
 
 if [ $1 = "up" ];
 then
-	curr_bg=$((curr_bg+5))
+	curr_bg=$((curr_bg+10))
 	if [ "$curr_bg" -gt "$max_bg" ];
 	then
 		curr_bg=$max_bg
 	fi
 else
-	curr_bg=$((curr_bg-5))
+	curr_bg=$((curr_bg-10))
 	if [ "$curr_bg" -lt 0 ];
 	then
 		curr_bg=0
